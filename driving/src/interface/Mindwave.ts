f7edf28ccd9ddc4a0ebd78eb3776bb485f09564d
Henrik Bråthen <henrik.brathen@bouvet.no>
2025-02-15 11:41:51 +0100

    Mindwave interface

---
+++ driving/src/interface/Mindwave.ts
@@ -0,0 +1,21 @@
+interface Mindwave {
+    timestamp: string;
+    poor_signal: number;
+    raw_value: number;
+    attention: number;
+    meditation: number
+    blink: number;
+    waves: Waves;
+}
+
+interface Waves {
+    delta: number;
+    theta: number;
+    "low-alpha": number;
+    "high-alpha": number;
+    "low-beta": number;
+    "high-beta": number;
+    "low-gamma": number;
+    "mid-gamma": number;
+}
+
