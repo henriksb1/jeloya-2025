60953502d899cf1c1d21166f823e9521d5e2e28f
Simen Burud <simen.burud@bouvet.no>
2025-02-15 10:16:52 +0100

    Working WS demo

--- python/ws-test.py
+++ python/ws-test.py
@@ -1,15 +1,21 @@
 import asyncio
+
 import websockets
 
+
 async def send_data(websocket):
+    print("New connection:", websocket)
     while True:
         data = "Hello from Python!"
         await websocket.send(data)
         await asyncio.sleep(1)
 
+
 async def main():
-    async with websockets.serve(send_data, "localhost", 8765):
+    async with websockets.serve(send_data, "0.0.0.0", 8765):
+        print("Serving")
         await asyncio.Future()  # run forever
+
 
 if __name__ == "__main__":
     asyncio.run(main())
